1a0708c7c11f5afec39c37be826762a0b9be91d8
moozzyk <moozzyk@gmail.com>
2023-10-07 21:51:59 -0700

    Refactor

--- shared.js
+++ shared.js
@@ -1,43 +1,47 @@
+export async function getAllDuplicateTabs() {
+  const allTabs = await chrome.tabs.query({});
+  const tabMap = new Map();
+  allTabs.forEach((tab) => {
+    if (!tabMap.has(tab.url)) {
+      tabMap.set(tab.url, []);
+    }
+    if (tab.active) {
+      // Put active tab at the front of the list so it's not closed
+      tabMap.get(tab.url).unshift(tab.id);
+    } else {
+      tabMap.get(tab.url).push(tab.id);
+    }
+  });
+  return tabMap;
+}
 
 async function getCurrentTab() {
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  if (!tab) {
+    // Somehow the above doesn't work for a tab that as gmail opened in it.
+    [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  }
   return tab;
 }
 
-export async function getDuplicateTabIds() {
+export async function getDuplicateTabIds(tabMap) {
     const currentTab = await getCurrentTab();
     if (!currentTab) {
       return [];
     }
 
-    let duplicateTabs = await chrome.tabs.query({url: currentTab.url});
-    let duplicateTabIds = duplicateTabs
-      .filter(tab => tab.id !== currentTab.id)
-      .map(tab => tab.id);
-    return duplicateTabIds;
-}
+    if (!tabMap) {
+      tabMap = await getAllDuplicateTabs();
+    }
 
-export async function getAllDuplicateTabs() {
-    const allTabs = await chrome.tabs.query({});
-    const tabMap = new Map();
-    allTabs.forEach((tab) => {
-      if (!tabMap.has(tab.url)) {
-        tabMap.set(tab.url, []);
-      }
-      if (tab.active) {
-        // Put active tab at the front of the list so it's not closed
-        tabMap.get(tab.url).unshift(tab.id);
-      } else {
-        tabMap.get(tab.url).push(tab.id);
-      }
-    });
-    return tabMap;
+    const duplicateTabs = tabMap.get(currentTab.url);
+    return duplicateTabs.slice(1);
 }
 
 export async function getDuplicateTabsCount() {
-  const activeTabDuplicatesCount = (await getDuplicateTabIds()).length;
   const duplicateTabMap = await getAllDuplicateTabs();
+  const activeTabDuplicatesCount = (await getDuplicateTabIds(duplicateTabMap)).length;
   let allDuplicateTabsCount = 0;
   duplicateTabMap.forEach((v) => {
     allDuplicateTabsCount += v.length - 1;
